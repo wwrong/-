3c5dbe658240592e6edeaaef208716c2a3b65fd2
guannan wang <warnerwrong@126.com>
2016-04-28 09:29:09 +0800

    Initial Commit

---
+++ 多线程回忆/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  多线程回忆
+//
+//  Created by Guan Nan Wang on 16/4/28.
+//  Copyright © 2016年 Guan Nan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 多线程回忆/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  多线程回忆
+//
+//  Created by Guan Nan Wang on 16/4/28.
+//  Copyright © 2016年 Guan Nan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
